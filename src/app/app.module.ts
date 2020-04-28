243a0c2598802ad108f0017d99988a598ec71ffc
john <wayaway.jtm@gmail.com>
2020-04-28 14:04:49 -0400

    init menu setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatMenuModule } from "@angular/material/menu";
+import { MatIconModule } from "@angular/material/icon";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeRouteComponent } from './home-route/home-route.component';
 import { AboutRouteComponent } from './about-route/about-route.component';
 import { ContactRouteComponent } from './contact-route/contact-route.component';
@@ -22,7 +24,9 @@ import { NotFoundRouteComponent } from './not-found-route/not-found-route.compon
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
