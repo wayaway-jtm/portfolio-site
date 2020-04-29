7b69423ae906f3eb91426f4e5dfb2038e5fde887
john <wayaway.jtm@gmail.com>
2020-04-29 18:51:38 -0400

    added routes to dropdown menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,38 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeRouteComponent } from './home-route/home-route.component';
+import { ContactRouteComponent } from './contact-route/contact-route.component';
+import { ProjectsRouteComponent } from './projects-route/projects-route.component';
+import { AboutRouteComponent } from './about-route/about-route.component';
+import { NotFoundRouteComponent } from './not-found-route/not-found-route.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: 'home',
+        component: HomeRouteComponent
+    },
+    {
+        path: 'about',
+        component: AboutRouteComponent
+    },
+    {
+        path: 'contact',
+        component: ContactRouteComponent
+    },
+    {
+        path: 'projects',
+        component: ProjectsRouteComponent
+    },
+    {
+        path: '',
+        component: HomeRouteComponent
+    },
+    {
+        path: '**',
+        component: NotFoundRouteComponent
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
