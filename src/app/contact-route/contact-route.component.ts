a9bcce068b9c47a54b1bec94564c6018d2b3c79a
john <wayaway.jtm@gmail.com>
2020-05-02 13:16:51 -0400

    Added 'show' option to phone num & changed show function to work for both email & phone

--- src/app/contact-route/contact-route.component.ts
+++ src/app/contact-route/contact-route.component.ts
@@ -8,13 +8,18 @@ import { Component, OnInit } from '@angular/core';
 export class ContactRouteComponent implements OnInit {
 
     public showEmail: boolean = false;
+    public showPhoneNum: boolean = false;
     constructor() { }
 
     ngOnInit(): void {
     }
 
-    toggleShowEmail() {
-        this.showEmail = !this.showEmail;
+    toggleShow(contactType: string) {
+        if (contactType.toLowerCase() === 'email') {
+            this.showEmail = !this.showEmail;
+        } else if (contactType.toLowerCase() === 'phone') {
+            this.showPhoneNum = !this.showPhoneNum;
+        }
     }
 
 }
